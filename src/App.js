cd93322a1cd37835fa23844701a81ca36ee02988
Himanshi Sharma <himanshi.sharma@hashedin.com>
2021-03-23 14:30:09 +0530

    Added local storage for persisting state after refresh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Modal } from 'antd';
+import { useEffect, useState } from "react";
+import { lists } from "./Mock";
 
 function App() {
+
+  const [list, setList] = useState(lists);  
+  const [draggedData, setDraggedData] = useState();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => { 
+    const savedListData = localStorage.getItem("lists");
+    savedListData ? setList(JSON.parse(savedListData)): updateDataInLocalStorage("lists", JSON.stringify(lists));
+  }, [])
+
+  const updateDataInLocalStorage = (key, value) => {
+    localStorage.setItem(key, value);
+  }
+  const addNewList = (titleData) => {
+    const addNewList = { title: titleData, items: []}; 
+    const updateList = [...list, addNewList];
+    setList(updateList);
+  }
+
+  const addNewItemInList = (title, assignee, listTitle) => {
+    const newItem = { itemTitle: title, assignedTo: assignee};
+    list.forEach((listData)=> {
+      if(listData.title===listTitle){
+        listData.items = [...listData.items,newItem];
+      }
+    })
+  }
+
+  const dragOver = (event) => {
+    event.preventDefault();
+  }
+
+  const dragStart = (event, itemId, listId) => {
+    event.dataTransfer.setData("itemId", itemId);
+    event.dataTransfer.setData("listId", listId);
+  }
+
+  const onDrop = (event, id) => {
+    const itemId = event.dataTransfer.getData("itemId");
+    const listId = event.dataTransfer.getData("listId");
+    if(draggedData.itemTitle === itemId) {
+      list.forEach((listArray) => {
+
+        if (listArray.title.toLowerCase() === listId.toLowerCase()) {
+            const updatedListItems = listArray.items.filter(item => item.itemTitle!==itemId);
+            listArray.items = [...updatedListItems];
+        }
+        if(listArray.title.toLowerCase() === id.toLowerCase()){
+          listArray.items = [...listArray.items, draggedData];
+          setDraggedData();
+        }
+    });  
+    }
+    updateDataInLocalStorage("lists", JSON.stringify(list));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="heading">Kanban</h1> 
+      <div className="button-container-view">  
+        <Button className="button-style" onClick={() => addNewList("titleData")}>Add List to the board</Button>
+        <Button className="button-style" onClick={() => { addNewItemInList("In title","asignee","In Review")}}>Reset Board</Button>
+      </div>
       </header>
+      <main className="main-container">
+        <div className="list-container"> 
+        {list && list.map((listItems)=> {
+          return  (<div key={listItems.title} className="list-style" onDragOver={(e)=> dragOver(e)} onDrop={(event) => onDrop(event, listItems.title)}>
+          <h2>{listItems.title}</h2>
+          {listItems.items.map((item) => {
+              return <div draggable key={item.itemTitle} className="list-item" onDragStart={(event)=> { setDraggedData(item); dragStart(event, item.itemTitle, listItems.title)}}>
+              <p className="item-title">{item.itemTitle}</p>
+              <span className="text">{`Assignee: ${item.assignedTo}`}</span>
+              </div>
+          })}
+         
+          </div>);
+        })}
+       </div>
+      </main>
+      <footer className="footer-section">All rights reserved</footer>
     </div>
   );
 }
---
+++ src/Mock.js
@@ -0,0 +1,14 @@
+export const lists= [
+    { title: "Backlog", 
+      items: [{itemTitle: "Create boilerplate", assignedTo: "Himanshi"}, {itemTitle: "Create git repo", assignedTo: "Himanshi"}]
+    },
+    { title: "In progress", 
+      items: [{itemTitle: "Create a ticket in jira for milestone 1.", assignedTo: "Himanshi"}, {itemTitle: "Create git repo", assignedTo: "Himanshi"}]
+    },
+    { title: "Dev complete", 
+      items: [{itemTitle: "Create boilerplate", assignedTo: "Himanshi"}, {itemTitle: "Create git repo", assignedTo: "Himanshi"}]
+    },
+    { title: "In Review", 
+      items: [{itemTitle: "Create boilerplate", assignedTo: "Himanshi"}, {itemTitle: "Create git repo", assignedTo: "Himanshi"}]
+    },
+];
